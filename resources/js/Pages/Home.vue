7c7fc00373c21be67ebe9bd32adfbe06815dd9ed
siniseong <shinhuiseong07@gmail.com>
2025-05-29 10:18:31 +0900

    feat :: 조건에 맞는 Vue + laravel 구성 설정

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="gonggoo" />
+    <h1>gonggoo with amuz Project</h1>
+    <p>Laravel + Vue 3 + Inertia.js</p>
+</template>
