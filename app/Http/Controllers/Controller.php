987e216cb8d0267c3e12a3687e23423296b02a06
huise0ng <shinhuiseong07@gmail.com>
2025-05-31 15:47:27 +0900

    feat :: swagger + api 환경 세팅

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,39 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+/**
+ * @OA\Info(
+ *     title="Gonggoo (공구) API",
+ *     version="0.1",
+ *     description="📦 학교 및 회사를 위한 공동 구매 서비스, 공구 API"
+ * )
+ */
+class Controller extends BaseController
 {
-    //
+    use AuthorizesRequests, ValidatesRequests;
+
+    /**
+     * @OA\Get(
+     *     path="/test",
+     *     summary="Swagger를 테스트 해봅니다.",
+     *     @OA\Response(
+     *         response=200,
+     *         description="Swagger + API 테스트 성공",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="Hello API!")
+     *         )
+     *     )
+     * )
+     */
+    public function test()
+    {
+        return response()->json([
+            'message' => 'Hello API!',
+            'status' => 'success'
+        ]);
+    }
 }
