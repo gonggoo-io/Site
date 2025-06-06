27b80e36dcf26b00ca791a63c994e0e76db58d0b
huise0ng <shinhuiseong07@gmail.com>
2025-06-06 21:53:09 +0900

    feat :: 1주일이 지난 항목은 조회시 보이지 않도록 구현

--- app/Models/Insert.php
+++ app/Models/Insert.php
@@ -3,11 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Carbon\Carbon;
 
 class Insert extends BaseModel
 {
+    use SoftDeletes;
+    
     protected $table = 'insert';
     protected $fillable = [
-        'user_id', 'item_name', 'link', 'participant_count', 'total_price'
+        'user_id', 'item_name', 'link', 'participant_count', 'total_price', 'created_at'
     ];   
+
+    protected static function booted()
+    {
+        static::addGlobalScope('not_expired', function ($query) {
+            $expiryDate = Carbon::now('Asia/Seoul')->subDays(7)->startOfSecond();
+            $query->where('created_at', '>=', $expiryDate);
+        });
+    }
 }
