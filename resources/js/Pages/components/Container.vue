e25fa47a484be05aa23fe35d3fabbe6afbe40f31
siniseong <shinhuiseong07@gmail.com>
2025-06-09 01:32:10 +0900

    fix :: 컨테이너 반응형 적용

--- resources/js/Pages/components/Container.vue
+++ resources/js/Pages/components/Container.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="w-full px-4">
-    <div class="max-w-[1440px] mx-auto flex justify-between items-center">
+  <div class="w-full px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12">
+    <div class="w-full sm:w-[640px] md:w-[768px] lg:w-[1024px] xl:w-[1100px] mx-auto flex justify-between items-center">
       <slot></slot>
     </div>
   </div>
