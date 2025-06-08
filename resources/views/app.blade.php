069f69ff9edf0871295a8312adc495d83c47dbb7
huise0ng <shinhuiseong07@gmail.com>
2025-06-08 15:31:55 +0900

    chore :: 헤더 완성

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,17 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <style>
+      @font-face {
+        font-family: 'Pretendard-Regular';
+        src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+        font-weight: 400;
+        font-style: normal;
+      }
+      * {
+        font-family: 'Pretendard', -apple-system, BlinkMacSystemFont, system-ui, Roboto, sans-serif;
+      }
+    </style>
     @vite('resources/js/app.js')
     @inertiaHead
   </head>
