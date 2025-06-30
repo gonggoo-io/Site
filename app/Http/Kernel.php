1a0cd26b0f631febbe67c3f1bb2069ec73ff02a6
siniseong <shinhuiseong07@gmail.com>
2025-06-30 20:15:00 +0900

    feat :: cors 모든 링크에 되도록 설정

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -7,7 +7,6 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 class Kernel extends HttpKernel
 {
     protected $middleware = [
-        \App\Http\Middleware\Cors::class,
         \Illuminate\Http\Middleware\HandleCors::class,
         \Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
