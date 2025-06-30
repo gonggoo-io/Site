f925b0c4f6d00579a0207c26f3d6798da51a0162
siniseong <shinhuiseong07@gmail.com>
2025-06-30 20:25:09 +0900

    Fix :: cors 에러 수정

--- config/cors.php
+++ config/cors.php
@@ -3,10 +3,10 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://gonggoo.kro.kr','https://gonggoo.kro.kr', 'http://127.0.0.1:5173', 'http://localhost:5173', 'http://localhost:8000'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 ]; 
--- vite.config.js
+++ vite.config.js
@@ -26,5 +26,6 @@ export default defineConfig({
     server: {
         host: 'localhost',
         port: 5173,
+        cors: true,
     },
 });
