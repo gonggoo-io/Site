b979197786e79430a695e63def1ae64ea89d5147
siniseong <shinhuiseong07@gmail.com>
2025-07-01 20:31:43 +0900

    chore :: 푸터 테스트 수정

--- resources/js/Pages/components/Footer.vue
+++ resources/js/Pages/components/Footer.vue
@@ -5,7 +5,7 @@
         <img :src="logoImage" alt="logo" class="w-5 h-5 filter grayscale opacity-60" />
         <div class="flex flex-col gap-1">
           <span class="text-xs text-gray-500">© 2025 gonggoo. All rights reserved.</span>
-          <span class="text-xs text-gray-500">Developed by <a href="https://github.com/siniseong" target="_blank" class="transition-colors">siniseong</a></span>
+          <span class="text-xs text-gray-500">Developed by <a href="https://github.com/siniseong" target="_blank" class="transition-colors">siniseong to amuz</a></span>
         </div>
       </div>
       <div class="flex items-center">
